fece2d3bc57f06c6868c98aef1f62d0044969469
enrico viola <enrico.viola@accenture.com>
2024-06-18 11:01:11 +0200

    add the enter trigger just in case

--- XRStrikeTeam/Assets/Scripts/SlideAnimatorController.cs
+++ XRStrikeTeam/Assets/Scripts/SlideAnimatorController.cs
@@ -8,8 +8,14 @@ namespace Accenture.XRStrikeTeam.Presentation
     public class SlideAnimatorController : MonoBehaviour
     {
         private Animator _animator = null;
+        private const string _paramEnterName = "Enter";
         private const string _paramExitName = "Exit";
+        private int _paramEnterIndex = -1;
         private int _paramExitIndex = -1;
+
+        public void EaseIn() {
+            _animator.SetTrigger(_paramEnterIndex);
+        }
 
         public void EaseOut() {
             _animator.SetTrigger(_paramExitIndex);
@@ -21,6 +27,7 @@ namespace Accenture.XRStrikeTeam.Presentation
 
             Misc.CheckNotNull(_animator);
 
+            _paramEnterIndex = Animator.StringToHash(_paramEnterName);
             _paramExitIndex = Animator.StringToHash(_paramExitName);
         }
     }
