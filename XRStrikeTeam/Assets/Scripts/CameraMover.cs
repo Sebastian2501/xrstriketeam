113d4a01b38566e9a046afb274c1d70b090cf1f6
enrico viola <enrico.viola@accenture.com>
2025-02-13 17:17:13 +0100

    state driven camera

--- XRStrikeTeam/Assets/Scripts/CameraMover.cs
+++ XRStrikeTeam/Assets/Scripts/CameraMover.cs
@@ -4,11 +4,18 @@ using Accenture.eviola.Async;
 using Accenture.eviola.Math;
 using UnityEngine;
 using UnityEngine.Events;
+using static UnityEditor.PlayerSettings;
 
 namespace Accenture.XRStrikeTeam.Presentation
 {
     public class CameraMover : MonoBehaviour
     {
+        public enum CameraState { 
+            ON_TARGET,
+            IN_TRAJECTORY,
+            ROTATING_TO_TARGET
+        }
+
         public UnityEvent<Pose> OnDestinationReached = new UnityEvent<Pose>();
 
         [Header("ExternalComponents")]
@@ -27,24 +34,92 @@ namespace Accenture.XRStrikeTeam.Presentation
         private float _fromTime = 0;
         private float _toTime = 0;
         private WaypointsTimedMovement _movement = new WaypointsTimedMovement();
+        private CameraState _curState = CameraState.ON_TARGET;
 
         #region Animation
 
-        public void Go(Vector3 pos, Quaternion rot, Trajectory trajectory = null) {
-            if (IsMoving()) return;
-            StopCamera();
+        public void Go(Vector3 pos, Quaternion rot, Trajectory trajectory = null) { 
+            StartTrajectory(pos, rot, trajectory);
+        }
+
+        public void SetCamera(Vector3 pos, Quaternion rot, bool instant = true) {
+            _targetPose.position = pos;
+            _targetPose.rotation = rot;
+            _srcPose.position = _camera.transform.position;
+            _srcPose.rotation = _camera.transform.rotation;
+            _fromTime = Time.time;
+            _toTime = _fromTime + _easingTime;
+            if (instant)
+            {
+                SetOnTarget(pos, rot);
+            }
+            else {
+                switch (State) {
+                    case CameraState.IN_TRAJECTORY:
+                        SetOnTarget(pos, _srcPose.rotation);
+                        StartRotateToTarget();
+                        break;
+                    case CameraState.ROTATING_TO_TARGET:
+                        
+                        break;
+                    case CameraState.ON_TARGET:
+                    default:
+                        SetOnTarget(pos, rot);
+                        break;
+                }
+            }
+        }
+
+        private void LoadTrajectory(Trajectory traj)
+        {
+            _movement.MovementEasing = traj.MovementEasing;
+            _movement.Duration = traj.Duration;
+            _movement.Waypoints.Add(_startPose);
+            if (traj.NumWaypoints > 2)
+            {
+                for (int i = 1; i < traj.NumWaypoints - 1; i++)
+                {
+                    Vector3 pos = traj.GetWayPoint(i);
+                    _movement.Waypoints.Add(new Pose(pos, eviola.Math.Vector.LookAt(pos, traj.GetWayPoint(i + 1))));
+                }
+            }
+            _movement.Waypoints.Add(_endPose);
+        }
+
+        private void HandleMovementDone(Timer.TimerStopType tst) {
+            if (tst == Timer.TimerStopType.MANUAL) return;
+            StartRotateToTarget();
+            OnDestinationReached.Invoke(_endPose);
+        }
+
+        #endregion
+
+        #region stateHandling
+        
+        public CameraState State { get { return _curState; } }
+
+        public bool IsMoving() { return State == CameraState.IN_TRAJECTORY; }
+
+        private void SetOnTarget(Vector3 pos, Quaternion rot) { 
+            _curState = CameraState.ON_TARGET;
+            _movement.Stop();
+            _camera.transform.position = pos;
+            _camera.transform.rotation = rot;
+        }
+
+        private void StartTrajectory(Vector3 posDest, Quaternion rotDest, Trajectory traj=null) {
+            _movement.Stop();
+            _curState = CameraState.IN_TRAJECTORY;
 
             _startPose.position = _camera.transform.position;
             _startPose.rotation = _camera.transform.rotation;
 
-            _endPose.position = pos;
-            _endPose.rotation = rot;
+            _endPose.position = posDest;
+            _endPose.rotation = rotDest;
 
-            _movement.Stop();
             _movement.AnimatedTransform = _camera.transform;
             _movement.Waypoints.Clear();
-
-            if (trajectory == null)
+            if (traj == null)
             {
                 _movement.Duration = _travelTime;
                 _movement.MovementEasing = Easing.LINEAR;
@@ -52,60 +127,42 @@ namespace Accenture.XRStrikeTeam.Presentation
                 _movement.Waypoints.Add(_endPose);
             }
             else { 
-                LoadTrajectory(trajectory);
+                LoadTrajectory(traj);
             }
-            
             _movement.Start();
         }
 
-        private void LoadTrajectory(Trajectory traj) {
-            _movement.MovementEasing = traj.MovementEasing;
-            _movement.Duration = traj.Duration;
-            _movement.Waypoints.Add(_startPose);
-            if (traj.NumWaypoints > 2) {
-                for (int i = 1; i < traj.NumWaypoints - 1; i++) {
-                    Vector3 pos = traj.GetWayPoint(i);
-                    _movement.Waypoints.Add(new Pose(pos, eviola.Math.Vector.LookAt(pos, traj.GetWayPoint(i+1))));
-                }
-            }
-            _movement.Waypoints.Add(_endPose);
-        }
+        private void StartRotateToTarget() {
+            _movement.Stop();
+            _curState = CameraState.ROTATING_TO_TARGET;
 
-        public void StopCamera() { _movement.Stop(); }
-
-        public void SetCamera(Vector3 pos, Quaternion rot, bool instant = true)
-        {
-            StopCamera();
-            _targetPose.position = pos;
-            _targetPose.rotation = rot;
-            _srcPose.position = _camera.transform.position;
-            _srcPose.rotation = _camera.transform.rotation;
             _fromTime = Time.time;
-            _toTime = _fromTime+_easingTime;
-            if (instant)
-            {
-                _camera.transform.position = pos;
-                _camera.transform.rotation = rot;
-            }
-        }
+            _toTime = _fromTime + _easingTime;
 
-        public bool IsMoving() { return _movement.IsRunning(); }
-
-        private void UpdateMovement() { 
-            _movement.Update();
-        }
-
-        private void HandleMovementDone(Timer.TimerStopType tst) {
-            if (tst == Timer.TimerStopType.MANUAL) return;
             _targetPose.position = _endPose.position;
             _targetPose.rotation = _endPose.rotation;
-            OnDestinationReached.Invoke(_endPose);
         }
 
-        private void UpdateTarget() {
-            if (_movement.IsRunning()) return;
-            if (Time.time > _toTime) return;
-            _camera.transform.rotation = Quaternion.Slerp(_srcPose.rotation, _targetPose.rotation, Remap.Map(Time.time, _fromTime, _toTime, 0,1));
+        private void UpdateRotateToTarget() {
+            if (Time.time > _toTime) {
+                SetOnTarget(_endPose.position, _endPose.rotation);
+                return;
+            }
+            _camera.transform.rotation = Quaternion.Slerp(_srcPose.rotation, _targetPose.rotation, Remap.Map(Time.time, _fromTime, _toTime, 0, 1));
+        }
+
+        private void UpdateState() {
+            switch (State) {
+                case CameraState.IN_TRAJECTORY:
+                    _movement.Update();
+                    break;
+                case CameraState.ROTATING_TO_TARGET:
+                    UpdateRotateToTarget();
+                    break;
+                case CameraState.ON_TARGET:
+                default:
+                    break;
+            }
         }
 
         #endregion
@@ -124,8 +181,7 @@ namespace Accenture.XRStrikeTeam.Presentation
 
         private void Update()
         {
-            UpdateMovement();
-            UpdateTarget();
+            UpdateState();
         }
         #endregion
     }
